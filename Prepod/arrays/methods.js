711965ecc1918abdef7b29c1512614b9fa5d9b76
Web <web@it.step>
2022-07-28 18:48:09 +0300

    arrays

---
+++ Prepod/arrays/methods.js
@@ -0,0 +1,73 @@
+// array methods
+
+// const arr = [23, 4, 56, 43, 0, 54, -23, 34, 23];
+// const arr2 = ['one', 'two', 'three'];
+// const arr3 = [false, true, true, false, true];
+
+// const resultArr = arr.concat([]); // копия массива
+// console.log(arr2.concat('hello world', false));
+
+// const resultArr = arr.concat(arr2);
+// const resultResultArr = resultArr.concat(arr3);
+
+// const arr = [ 4, 56, 34, 43, 0, 54, -23, 34, 23];
+
+// const arr3 = [false, true, true, false, true];
+// const arr4 = [ 1,51,2,56, 89, 45, 1,3,4,5 ];
+
+// const result = arr2.push(100);
+// const result = arr2.pop();
+// const result = arr2.shift();
+// const result = arr2.unshift('smth', 'smth else');
+// .indexOf()
+// .lastIndexOf()
+
+// arr2.reverse();
+// arr.sort();
+// const result  = arr2.slice(2);
+
+// const arr2 = ['one', 'two', 'three', 'two', 'zzzzz', 'xxxxx', 'ccccc', 'vvvvvv'];
+
+// const result = arr2.splice( 2 , 2,  "VALERCHIK", "VALERCHIK");
+
+// console.log(result);
+
+// const result = arr2.join(' ');
+
+// STRING METHOD!!!!!!!!!!
+
+// const str = 'i love ITSTEP very much';
+
+// console.log( str.split('v') );
+
+// rest, spread, destructuring
+
+// console.log( Math.max( ...arr4 ) );
+
+// const arr2 = ['one', 'two', 'three', 'two', 'zzzzz', 'xxxxx', 'ccccc', 'vvvvvv'];
+// const arr4 = [ 1,51,2,56, 89, 45, 1,3,4,5 ];
+
+// const arr5 = [ 20, ...arr4, undefined, false];
+// const arr6 = [...arr2, ...arr4];
+
+// arr5[0] = 100;
+
+// console.log(arr4);
+
+const arr = ["one", "two", "three"];
+const test = [];
+
+// const a = arr[0];
+// const b = arr[1];
+// const c = arr[2];
+
+// let [ a = 'default value for a', b = 'default value' ] = test;
+
+// const [atesttttt, b, ...blabla] = arr;
+
+// if (2 > 1) {
+//   for (;;) {
+//     break;
+//   }
+//   console.log();
+// } else console.log();
